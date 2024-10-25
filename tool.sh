ce70bd144cc9012aca39c8c2e0c2ad3aa98b39dd
Gourmet <110063472+ItalianG0urmet@users.noreply.github.com>
2024-10-25 14:18:20 +0000

    Create tool.sh

---
+++ tool.sh
@@ -0,0 +1,34 @@
+pid=$(pidof java)
+
+check_pid() {
+  if kill -0 "$pid" 2>/dev/null; then
+    fast_check
+  else
+    echo "Unknown PID"
+  fi
+}
+
+get_time_info() {
+  last_deleted_file_date=$(ls -lt ~/.local/share/Trash/ | awk 'NR==2 {print $8}')
+  minecraft_start_date=$(ps -p $pid -o etime= | tr -d '\n' | awk '{ if (NF==2) { print $1 } else { print $1 }}')
+  minecraft_execution_date=$(ps -p $pid -o lstart= | cut -d' ' -f4-6)
+
+  echo "Last deleted file at: [$last_deleted_file_date]"
+  echo "Minecraft started at: [$minecraft_execution_date]"
+  echo "Minecraft execution time: [$minecraft_start_date]"
+}
+
+list_active_mods() {
+  echo "Mods in the process with PID $pid:"
+  lsof -p "$pid" 2>/dev/null | grep '/mods/.*\.jar$' | awk -F '/mods/' '{print $2}'
+}
+
+fast_check() {
+  echo "----------------------"
+  get_time_info
+  echo "----------------------"
+  list_active_mods
+  echo "----------------------"
+}
+
+check_pid
